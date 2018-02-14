371cb3f2f3a618163e502dfefba16e9634a6980a
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-02-14 19:30:27 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
